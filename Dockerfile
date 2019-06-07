2d6c9591e28e5d7c17cc3d6fba77d4729975d358
Markus Hederström <markus.hederstrom@humany.com>
2019-06-07 22:30:32 +0200

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:10.11-alpine
 
 ENV NPM_CONFIG_LOGLEVEL notice
-ENV DATABASE_URL postgres://urqxkcucomqdik:bd253d3726c318488b6194380b44c1a30f5d55a89d45a32459ee917300d1d18b@ec2-176-34-184-174.eu-west-1.compute.amazonaws.com:5432/d9s15jo0i53j8l
+ENV DATABASE_URL postgresql://donami:Nu7XX7TqUCqyNPlDgbdk@spotmovie.cxcuwe3zi32g.us-east-2.rds.amazonaws.com/spotmovie
 ENV CLIENT_URL http://18.216.230.185
 
 # Install NPM packages
--- deploy_app.sh
+++ deploy_app.sh
@@ -11,23 +11,30 @@
 set -e
 
 DOCKER_IMAGE=$1
-CONAINER_NAME="spot-movie-backend"
+CONTAINER_NAME="spot-movie-backend"
 
-# Check for arguments
-if [[ $# -lt 1 ]] ; then
-    echo '[ERROR] You must supply a Docker Image to pull'
-    exit 1
-fi
+# # Check for arguments
+# if [[ $# -lt 1 ]] ; then
+#     echo '[ERROR] You must supply a Docker Image to pull'
+#     exit 1
+# fi
 
-echo "Deploying SpotMovie Backend to Docker Container"
+# echo "Deploying SpotMovie Backend to Docker Container"
 
-#Check for running container & stop it before starting a new one
-if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
-    docker stop spot-movie-backend
-fi
+# #Check for running container & stop it before starting a new one
+# if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "true" ]; then
+#     docker stop spot-movie-backend
+# fi
 
-echo "Starting SpotMovie Backend using Docker Image name: $DOCKER_IMAGE"
+# echo "Starting SpotMovie Backend using Docker Image name: $DOCKER_IMAGE"
 
-docker run -d --rm=true -p 3030:3030  --name spot-movie-backend $DOCKER_IMAGE
+# docker run -d --rm=true -p 3030:3030  --name spot-movie-backend $DOCKER_IMAGE
 
-docker ps -a
+# docker ps -a
+
+# docker pull donami/spot-movie-backend:latest  
+# docker stop spot-movie-backend
+# docker rm spot-movie-backend
+# docker rmi donami/spot-movie-backend:current  
+# docker tag donami/spot-movie-backend:latest donami/spot-movie-backend:current  
+# docker run -d --name spot-movie-backend donami/spot-movie-backend:latest  
